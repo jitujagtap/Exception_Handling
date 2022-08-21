837440f66b3bd5e73767a96ea3d304822c9277bc
jitujagtap <56694040+jitujagtap@users.noreply.github.com>
2022-08-21 22:28:13 +0530

    Add files via upload

---
+++ Exceptions_String to Integer.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    try:
+        S = input(3)
+        s = int(S)
+        print(s)
+    except:
+        print("Bad String")
